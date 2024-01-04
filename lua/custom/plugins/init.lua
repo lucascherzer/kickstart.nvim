ac4b237c621a8162c8bd3794822b81abbe00ad69
luca scherzer <luca.scherzer@lucas-MacBook-Pro.local>
2024-01-04 12:17:20 +0100

    remove plugin with duplicate capability

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,16 +3,6 @@
 --
 -- See the kickstart.nvim README for more information
 return {
- {
-      "kylechui/nvim-surround",
-      version = "*", -- Use for stability; omit to use `main` branch for the latest features
-      event = "VeryLazy",
-      config = function()
-	  require("nvim-surround").setup({
-	      -- Configuration here, or leave empty to use defaults
-	  })
-      end
-  },
   {
       'windwp/nvim-autopairs',
       event = "InsertEnter",
